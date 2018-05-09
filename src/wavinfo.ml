529e39f9059c71d7deebaf7183900e749f8d160f
Romain Beauxis <toots@rastageeks.org>
2018-05-09 10:47:11 -0500

    Added simple demo binary.

---
+++ src/wavinfo.ml
@@ -0,0 +1,20 @@
+open BsCallback
+
+external argv : string array = "" [@@bs.val] [@@bs.scope "process"]
+
+external exit : int -> 'a = "" [@@bs.val] [@@bs.scope "process"]
+
+external stringify : 'a Js.t -> unit Js.Nullable.t -> int -> string = "" [@@bs.val] [@@bs.scope "JSON"]
+
+let () =
+ let path =
+  try
+   argv.(2)
+  with _ ->
+    Js.log "Usage: wavinfo /path/to/file.wav";
+    exit 1
+ in
+ finish (Wav.read path >> fun info ->
+   let info = stringify info Js.Nullable.null 2 in
+   Js.log {j|Wav file info:\n$(info)|j};
+   return ())
